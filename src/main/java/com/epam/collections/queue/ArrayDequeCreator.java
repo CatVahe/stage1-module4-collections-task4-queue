85bcc0ba6a3349196a8d5d34754dac4b25198306
Vahe <vahe.jaloyan34@gmail.com>
2023-10-24 23:06:00 +0400

    ready

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -6,5 +6,21 @@ import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+       ArrayDeque<Integer> result = new ArrayDeque<>();
+
+        result.offerLast(firstQueue.poll());
+        result.offerLast(firstQueue.poll());
+        result.offerLast(secondQueue.poll());
+        result.offerLast(secondQueue.poll());
+
+        while (!firstQueue.isEmpty() || !secondQueue.isEmpty()) {
+            firstQueue.offer(result.pollLast());
+            result.offerLast(firstQueue.poll()); //get first, add last
+            result.offerLast(firstQueue.poll()); //get first, add last
+            secondQueue.offer(result.pollLast());
+            result.offerLast(secondQueue.poll()); //get first, add last
+            result.offerLast(secondQueue.poll()); //get first, add last
+        }
+       return result;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,22 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+
+        List<Integer> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            queue.add(i);
+        }
+        int index = 0;
+        while (!queue.isEmpty()) {
+            index = (index + everyDishNumberToEat - 1) % queue.size();
+            result.add(queue.remove(index));
+        }
+        return result;
+
     }
 }
